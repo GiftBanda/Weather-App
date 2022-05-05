e0886f992ab3c1e23bbb123b499d228d8063f228
GiftBanda <bandagift42@gmail.com>
2022-05-05 11:13:39 +0200

    ui changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,14 @@ import { useState, useEffect } from "react";
 const Home = () => {
 
     const [weather, setWeather] = useState(null)
-    const [isLoading, setIsLoading] = useState(false)
+    // const [isLoading, setIsLoading] = useState(false)
     const [error, setError] = useState('')
     const [city, setCity] = useState('Pretoria')
 
     const getWeather = async () => {
         // e.preventDefault()
        
-        const api_call = await fetch(`http://localhost:5000/api?q=${city}`, {
+        const api_call = await fetch(`http://localhost:3001/api?q=${city}`, {
             accept: 'application/json'
         })
         const data = await api_call.json()
@@ -21,7 +21,7 @@ const Home = () => {
             setError('')
            console.log(data)
         }
-        setIsLoading(false)
+        //setIsLoading(false)
     }
 
     useEffect(() => {
@@ -38,16 +38,18 @@ const Home = () => {
 
     return(
         
-        <div>
+        <div className="container">
+            <img  className="wallpaper" src='../images/rain.jpg' alt="weather icon" />
             {error && <div className="error">{error}</div>}
             {weather === null ? <div><
                 h1>Loading.....</h1>
                
-             </div>: <div><h1>Home Page</h1>
+             </div>: <div className="card"><h1>Weather App</h1>
             <h3>{weather.name === null ? '' : weather.name}</h3>
-            <h3>Wind Speed: {weather.wind.speed === null ? '' : weather.wind.speed}</h3>
-            <h3>Temperature: {weather.main.temp === null ? '': weather.main.temp}</h3>
-            <h3>Clouds: {weather.weather[0].description === null ? '' : weather.weather[0].description}</h3>
+            
+            <h2>Wind Speed: {weather.wind.speed === null ? '' : weather.wind.speed}</h2>
+            <h2>Temperature: {weather.main.temp === null ? '': weather.main.temp}</h2>
+            <h2>Clouds: {weather.weather[0].description === null ? '' : weather.weather[0].description}</h2>
             <form onSubmit={onSubmit}>
                 <input type="text" name="city" placeholder="City" onChange={(e) => setCity(e.target.value) }/>
                 <button onClick={getWeather}>Get Weather</button>
